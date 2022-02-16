7acfe2cf026658ddf24a65248c236647530be611
miyaichi <burnedsoba@gmail.com>
2022-02-16 23:12:13 +0900

    [Fix]mypage/activities_controllerのミスを修正

--- app/controllers/mypage/activities_controller.rb
+++ app/controllers/mypage/activities_controller.rb
@@ -1,5 +1,5 @@
 class Mypage::ActivitiesController < ApplicationController
-  before_action :require_login only: %i[index]
+  before_action :require_login, only: %i[index]
   def index
     @activities = current_user.activities.order(created_at: :desc).page(params[:page]).per(10)
   end
