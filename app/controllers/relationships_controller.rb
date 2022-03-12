8f3491ca7e24bcb9bbcade728aff3462ccbfc327
miyaichi <burnedsoba@gmail.com>
2022-03-12 22:29:50 +0900

    [Add]通知を送るための判定を追加

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,7 +4,7 @@ class RelationshipsController < ApplicationController
   def create
     # リクエストに含まれる（フォローする人の）IDからフォローするユーザーを取得
     @user = User.find(params[:followed_id])
-    UserMailer.with(user_from: current_user, user_to: @user).follow.deliver_later if current_user.follow(@user)
+    UserMailer.with(user_from: current_user, user_to: @user).follow.deliver_later if current_user.follow(@user) && @user.notification_on_follow?
   end
 
   def destroy
